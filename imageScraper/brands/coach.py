168b39b9826b2edbc09905f07da0537a3562fe03
LeiG <LeiG.inbox@gmail.com>
2015-12-13 15:08:52 -0800

    Include scrapers for brands

---
+++ imageScraper/brands/coach.py
@@ -0,0 +1,59 @@
+"""
+Image scraper specifically for www.coach.com
+"""
+import re
+import math
+from collections import defaultdict
+from bs4 import BeautifulSoup
+
+from ..base import BaseScraper
+
+HOMEURL = 'http://www.coach.com/'
+BRAND = 'Coach'
+
+
+class CoachScraper(BaseScraper):
+    def getCategoryList(self, bsObj):
+        for cat in bsObj.findAll('a', {'data-id':re.compile('^category-'), 'class':'level-2'}):
+            catName = re.sub('category-', '', cat.attrs['data-id'])
+            self.categoryList[catName] = cat.attrs['href']
+
+    def getProductList(self, bsObj):
+        productList = defaultdict(str)
+        for prod in bsObj.findAll('h2'):
+            if prod.find('a', {'class':'name-link'}) is not None:
+                prodUrl = prod.find('a').attrs['href']
+                try:
+                    prodName = re.search('/([A-Za-z0-9]+?)\.html', prodUrl).group(1)
+                    productList[prodName] = prodUrl
+                except AttributeEr:
+                    pass
+        return productList
+
+    def getImageList(self, bsObj):
+        imageList = defaultdict(str)
+        imageCounter = 0
+        for img in bsObj.findAll('img', {'data-large-image':re.compile('.+')}):
+            if img.attrs['class'][0] != 'set-to-fullscreen':
+                imageList[str(imageCounter)] = img.attrs['data-large-image']
+                imageCounter += 1
+        return imageList
+
+    def getPrice(self, bsObj):
+        # if on sale
+        try:
+            price = bsObj.find('span', {'class':'standardprice'}).text
+            price = re.search(r'\$([0-9]+?)\n', price, re.M).group(1)
+
+            salePrice = bsObj.find('span', {'class':'salesprice'}).text
+            salePrice = re.search(r'\$([0-9]+?)\n', salePrice, re.M).group(1)
+
+            return math.ceil(float(price)), math.ceil(float(salePrice))
+        # not on sale
+        except:
+            price = bsObj.find('span', {'class':'price-sales'}).text
+            price = re.search(r'\$([0-9]+?)\n', price, re.M).group(1)
+
+            salePrice = price
+            return math.ceil(float(price)), price
+
---
+++ imageScraper/brands/katespade.py
@@ -0,0 +1,58 @@
+"""
+Image scraper specifically for www.coach.com
+"""
+import re
+import math
+from collections import defaultdict
+from bs4 import BeautifulSoup
+
+from ..base import BaseScraper
+
+HOMEURL = 'https://www.katespade.com/'
+BRAND = 'KateSpade'
+
+
+class KateSpadeScraper(BaseScraper):
+    def getCategoryList(self, bsObj):
+        for cat in bsObj.findAll('a', {'class':'level-2'}):
+            catUrl = cat.attrs['href']
+            if 'view-all' in catUrl:
+                catName = re.search('/([a-z\-]+?)/view\-all', catUrl).group(1)
+                self.categoryList[catName] = catUrl
+
+    def getProductList(self, bsObj):
+        productList = defaultdict(str)
+        for prod in bsObj.findAll('a', {'class':'name-link'}):
+            prodUrl = prod.attrs['href']
+            try:
+                prodName = re.search('/([A-Za-z0-9]+?)\.html', prodUrl).group(1)
+                productList[prodName] = prodUrl
+            except AttributeError:
+                pass
+        return productList
+
+    def getImageList(self, bsObj):
+        imageList = defaultdict(str)
+        imageCounter = 0
+        for img in bsObj.findAll('a', {'class':'thumbnail-link'}):
+            imageList[str(imageCounter)] = img.attrs['href']
+            imageCounter += 1
+        return imageList
+
+    def getPrice(self, bsObj):
+        # if on sale
+        try:
+            price = bsObj.find('span', {'class':'price-standard'}).text
+            price = re.search('\$([0-9\.]+?)$', price).group(1)
+
+            salePrice = bsObj.find('span', {'class':'price-sales'}).text
+            salePrice = re.search('\$([0-9\.]+?)$', salePrice).group(1)
+
+            return math.ceil(float(price)), math.ceil(float(salePrice))
+        # not on sale
+        except:
+            price = bsObj.find('span', {'class':'price-sales'}).text
+            price = re.search('\$([0-9\.]+?)$', price).group(1)
+
+            salePrice = price
+            return math.ceil(float(price)), math.ceil(float(salePrice))
